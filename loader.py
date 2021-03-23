2dbf86f1c7ff5fce3e6dd8bb162f82100cc51aa2
alexoff <davydovaleksey932@gmail.com>
2021-03-24 00:01:36 +1000

    added func to get tasks

--- loader.py
+++ loader.py
@@ -1,9 +1,8 @@
 # from os import getenv
 
 from aiogram import Bot, Dispatcher, types
-from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.contrib.fsm_storage.redis import RedisStorage2
 
 bot = Bot(token='1679438623:AAHEhDonSfN08ZYbB3svi95rOh9R9hucsZg', parse_mode=types.ParseMode.HTML)
-storage = MemoryStorage()
+storage = RedisStorage2()
 dp = Dispatcher(bot, storage=storage)
